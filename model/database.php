c106b900b52c3c739754e18586d8f8efbe8fa6f9
adbookerfhsu <58403630+adbookerfhsu@users.noreply.github.com>
2020-03-22 14:58:03 -0500

    revert back to local host for db connection

--- model/database.php
+++ model/database.php
@@ -1,12 +1,11 @@
 <?php
-    $dsn = "mysql:host=	hngomrlb3vfq3jcr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=kgp21rpv7ij4y8mf";
-    $username = "pwptmv8c3plijz4l";
-    $password ="upx9tlzf0xdvnlq9";
+    $dsn = "mysql:host=localhost;dbname=zippyusedautos";
+    $username = "root";
     
     
 
 try{
-    $db = new PDO($dsn, $username, $password);
+    $db = new PDO($dsn, $username);
 } catch (PDOException $e) {
     $error_message = $e->getMessage();
     include ('erros/database_error.php');
