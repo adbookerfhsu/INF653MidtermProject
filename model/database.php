5914f32ae620190d517c4bbb46d959218b4b6a54
adbookerfhsu <58403630+adbookerfhsu@users.noreply.github.com>
2020-03-22 15:08:12 -0500

    update heroku credentials

--- model/database.php
+++ model/database.php
@@ -1,11 +1,12 @@
 <?php
-    $dsn = "mysql:host=localhost;dbname=zippyusedautos";
-    $username = "root";
+    $dsn = "mysql:host=	hngomrlb3vfq3jcr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=	kgp21rpv7ij4y8mf";
+    $username = "pwptmv8c3plijz4l";
+    $password = "upx9tlzf0xdvnlq9";
     
     
 
 try{
-    $db = new PDO($dsn, $username);
+    $db = new PDO($dsn, $username, $password);
 } catch (PDOException $e) {
     $error_message = $e->getMessage();
     include ('erros/database_error.php');
